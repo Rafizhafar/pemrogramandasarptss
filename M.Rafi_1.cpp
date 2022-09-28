c81c7f75628f6ee26cfcfb8c9cf3e1e2c32bd0ce
Rafizhafar <111164558+Rafizhafar@users.noreply.github.com>
2022-09-28 16:48:20 +0700

    Add files via upload

---
+++ M.Rafi_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{  
+    cout << "Nama : M.Rafi Zhafar Muzakki Syah" << endl;
+    cout << "Kelas : X RPL 1" << endl;
+    cout << "Nilai: 95"<< endl;
+    
+ 
+}
+
